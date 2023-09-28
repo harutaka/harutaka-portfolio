c30718d543e7172f44b0337b6ca3c6b7e6c51d98
harutaka <10650294+harutaka@users.noreply.github.com>
2023-09-28 20:24:09 +0900

    ContactとFooter追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja" data-theme="light" className={montSerrat.className}>
+    <html lang="ja" data-theme="light" className={`${montSerrat.className} scroll-smooth`}>
       <body>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
-import Banner from '@/components/Banner'
-import Header from '@/components/Header'
-import Profile from '@/components/Profile'
-import Skill from '@/components/Skill'
-import React from 'react'
+import Banner from "@/components/Banner"
+import Contact from "@/components/Contact"
+import Footer from "@/components/Footer"
+import Header from "@/components/Header"
+import Profile from "@/components/Profile"
+import Skill from "@/components/Skill"
+import React from "react"
 
 const Home = () => {
   return (
     <>
       <Banner />
-      <div className='container mx-auto'>
+      <div className="container mx-auto">
         <Header />
         <Profile />
         <Skill />
+        <div className="h-96 bg-blue-400 my-8"></div>
+        <div className="h-96 bg-blue-500 my-8"></div>
+        <div className="h-96 bg-orange-400 my-8"></div>
 
-        <div className='h-96 bg-blue-400 my-8'></div>
-        <div className='h-96 bg-blue-500 my-8'></div>
-        <div className='h-96 bg-orange-400 my-8'></div>
+        <Contact />
       </div>
+        <Footer />
     </>
   )
 }
