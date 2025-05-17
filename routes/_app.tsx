60bf9bd8fa86c4953fc31fdeffee8f59ce012b6e
harutaka <10650294+harutaka@users.noreply.github.com>
2025-05-17 09:50:22 +0000

    fix metadata

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,17 +4,15 @@ export default function App({ Component }: PageProps) {
     <html lang="ja" data-theme="light">
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="harutaka portfolio" />
-        <meta property="og:url" content="https://harutaka-portfolio.vercel.app" />
-        <meta property="og:description" content="はるたかのポートフォリオサイトです。JavaScript系のWebアプリ制作はお任せください。" />
-        <meta property="og:image" content="https://harutaka-portfolio.vercel.app/img/works/portfolio_overview.jpg" />
-        <meta name="description" content="はるたかのポートフォリオサイトです。JavaScript系のWebアプリ制作はお任せください。" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>harutaka portfolio</title>
         <link rel="stylesheet" href="/styles.css" />
-        <link rel="canonical" href="https://harutaka-portfolio.vercel.app" />
+        <meta name="description" content="はるたかのポートフォリオサイトです。JavaScript系のWebアプリ制作はお任せください。" />
+
+        <meta property="og:title" content="harutaka portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://harutaka-portfolio.vercel.app" />
+        <meta property="og:image" content="https://harutaka-portfolio.vercel.app/img/works/portfolio_overview.jpg" />
       </head>
       <body>
         <Component />
