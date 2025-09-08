3b7a3d346866dadf1f706a013a6e7e90be3f5739
harutaka <10650294+harutaka@users.noreply.github.com>
2025-09-08 17:03:09 +0900

    Re move Next.js framework (#7)
    * Revert "Merge pull request #3 from harutaka/pr/change-deno-fresh-flamework"
    
    This reverts commit f45f64d8be97cbf6a693ef95ebe4a9ad2006e201.
    
    * delete using deno skill
    
    * delete deno deploy workflow
    
    * delete twitter meta data

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # harutaka-portfolio
-
 ポートフォリオサイト
--- components/works/PWorks.tsx
+++
@@ -1,7 +0,0 @@
-import { FunctionComponent } from "preact"
-
-const PWorks: FunctionComponent<unknown> = (props) => {
-  return <p class="mb-8">{props.children}</p>
-}
-
-export default PWorks
