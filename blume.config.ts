419990c2048f797d1361a67763fa5ce47a1e2dcc
harutaka <10650294+harutaka@users.noreply.github.com>
2026-08-11 21:00:57 +0900

    chore: delete github settings

--- blume.config.ts
+++ blume.config.ts
@@ -5,12 +5,6 @@ export default defineConfig({
   description: "はるたかのポートフォリオサイトです。Node.js系のWebアプリ制作はお任せください。",
   feedback: false,
 
-  github: {
-    owner: "harutaka",
-    repo: "harutaka-portfolio",
-    branch: "main",
-  },
-
   deployment: {
     site: "https://harutaka.github.io",
     base: "/harutaka-portfolio",
